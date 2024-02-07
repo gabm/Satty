74dbe187a21704ff4a3878a3c39623430c900bf1
Matthias Gabriel <gabm+pub@mailbox.org>
2024-02-07 12:17:31 +0100

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,41 @@
-# in flake.nix
 {
+  description = "A basic Rust devshell for NixOS users developing gtk/libadwaita apps";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell
-            {
-              buildInputs = [
-                (pkgs.rust-bin.stable.latest.rust.override {
-                  extensions = [ "rust-src" ];
-                })
-              ];
-              packages = [
-                pkg-config
-                pango
-                gtk4
-                libadwaita
-              ];
-            };
-        }
-      );
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            pkg-config
+
+            gtk4
+            wrapGAppsHook4 # this is needed for relm4-icons to properly load after gtk::init()
+            libadwaita
+
+            (rust-bin.stable.latest.default.override
+              {
+                extensions = [ "rust-src" ];
+              })
+          ];
+
+          shellHook = ''
+            export GSETTINGS_SCHEMA_DIR=${glib.getSchemaPath gtk4}
+          '';
+        };
+      }
+    );
 }
